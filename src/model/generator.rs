1448b129875a417746bd07ef05f212900164a2f3
Maxim Gribov (mcsim4s) <mcsim1993@gmail.com>
2023-09-11 19:55:50 +0300

    pawns double move

--- src/model/generator.rs
+++ src/model/generator.rs
@@ -1,4 +1,4 @@
-use crate::model::*;
+use crate::{model::*, util};
 use static_data::*;
 
 impl Board {
@@ -7,6 +7,13 @@ impl Board {
             WHITE
         } else {
             BLACK
+        }
+    }
+    pub fn inactive_color(&self) -> u8 {
+        if self.white_is_active {
+            BLACK
+        } else {
+            WHITE
         }
     }
 
@@ -30,7 +37,7 @@ impl Board {
             }
         }
 
-        buffer.into_iter().filter(|&x| !self.squares[x.to].is_color(active_color)).collect()
+        buffer
     }
 
     fn gen_piece_moves(&self, piece: u8, pos: usize) -> Vec<Move> {
@@ -52,8 +59,10 @@ impl Board {
     }
 
     fn knight_moves(&self, pos: usize) -> Vec<Move> {
+        let active_color = self.active_color();
         KNIGHT_MOVES[pos]
             .into_iter()
+            .filter(|&&x| !self.squares[x as usize].is_color(active_color))
             .map(|&x| Move {
                 from: pos,
                 to: x,
@@ -65,6 +74,7 @@ impl Board {
     fn sliding_moves(&self, pos: usize, directions: std::ops::Range<usize>) -> Vec<Move> {
         let mut result: Vec<Move> = Vec::new();
         let active_color = self.active_color();
+        let inactive_color = self.inactive_color();
         for direction in directions {
             for distance in 1..EDGE_DISTANCE[pos][direction] + 1 {
                 let to = (pos as i8 + DIRECTIONS[direction] * distance) as usize;
@@ -75,41 +85,69 @@ impl Board {
                     from: pos,
                     to,
                     promote_to: None,
-                })
+                });
+                if self.squares[to].is_color(inactive_color) {
+                    break;
+                }
             }
         }
         result
     }
 
-    fn king_moves(&self, king_index: usize) -> Vec<Move> {
-        let candidates: [Option<usize>; 8] = [
-            king_index.checked_sub(9),
-            king_index.checked_sub(8),
-            king_index.checked_sub(7),
-            king_index.checked_sub(1),
-            king_index.checked_add(1),
-            king_index.checked_add(7),
-            king_index.checked_add(8),
-            king_index.checked_add(9),
-        ];
-        candidates
-            .into_iter()
-            .flatten()
-            .filter(|&x| x < 64)
-            .map(|x| Move {
-                from: king_index,
-                to: x,
-                promote_to: None,
-            })
-            .collect()
+    fn king_moves(&self, pos: usize) -> Vec<Move> {
+        let mut result: Vec<Move> = Vec::new();
+        let active_color = self.active_color();
+        for direction in 0..8 {
+            if EDGE_DISTANCE[pos][direction] > 0 {
+                let to = (pos as i8 + DIRECTIONS[direction]) as usize;
+                if !self.squares[to].is_color(active_color) {
+                    result.push(Move {
+                        from: pos,
+                        to,
+                        promote_to: None,
+                    });
+                }
+            }
+        }
+        result
     }
 
     fn pawn_moves(&self, pos: usize) -> Vec<Move> {
-        let regular_move = if self.active_color() == pieces::WHITE { pos - 8 } else { pos + 8 };
-        vec![Move {
-            from: pos,
-            to: regular_move,
-            promote_to: None,
-        }]
+        let mut result: Vec<Move> = Vec::new();
+        let active_color = self.active_color();
+        let diff: i8 = if active_color == pieces::WHITE { -8 } else { 8 };
+        let regular_move = (pos as i8 + diff) as usize;
+        if self.squares[regular_move] == pieces::NONE {
+            result.push(Move {
+                from: pos,
+                to: regular_move,
+                promote_to: None,
+            });
+        }
+        if pos % 8 != 0 && self.squares[regular_move - 1].is_color(self.inactive_color()) {
+            result.push(Move {
+                from: pos,
+                to: regular_move - 1,
+                promote_to: None,
+            });
+        }
+        if pos % 8 != 7 && self.squares[regular_move + 1].is_color(self.inactive_color()) {
+            result.push(Move {
+                from: pos,
+                to: regular_move + 1,
+                promote_to: None,
+            });
+        }
+        let rank = pos / 8;
+        let double_move_avaliable = (rank == 1 && active_color == BLACK) || (rank == 6 && active_color == WHITE);
+        let double_move = (regular_move as i8 + diff) as usize;
+        if double_move_avaliable && result.len() > 0 && self.squares[double_move] == NONE {
+            result.push(Move {
+                from: pos,
+                to: double_move,
+                promote_to: None,
+            });
+        }
+        result
     }
 }
